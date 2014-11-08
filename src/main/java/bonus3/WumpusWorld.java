8fcaed5e419eebe9173560968f8007d7dc7af9e1
Mann <chanmannlim@gmail.com>
2014-11-08 16:39:14 -0600

    7.3 b

--- src/main/java/bonus3/WumpusWorld.java
+++ src/main/java/bonus3/WumpusWorld.java
@@ -4,28 +4,44 @@ import aima.core.logic.propositional.kb.KnowledgeBase;
 
 public class WumpusWorld {
     
-    private KnowledgeBase kb = new KnowledgeBase();
-    
     public void print_7_3_a() {
+        KnowledgeBase kb = new KnowledgeBase();
+
         kb.tell("B11 <=> P12 | P21");
+        kb.tell("OK11 <=> ~P11");
         kb.tell("OK12 <=> ~P12");
         kb.tell("OK21 <=> ~P21");
 
+        kb.tell("OK11");
         kb.tell("~B11");
-        infer(new String[] { "OK12", "OK21" });
+        infer(kb, new String[] { "OK12", "OK21"});
     }
     
-    private void infer(String[] queries) {
+    public void print_7_3_b() {
+        KnowledgeBase kb = new KnowledgeBase();
+
+        kb.tell("B21 <=> P11 | P31 | P22");
+        kb.tell("OK11 <=> ~P11");
+        kb.tell("OK12 <=> ~P12");
+        kb.tell("OK21 <=> ~P21");
+        
+        kb.tell("B21");
+        kb.tell("OK11");
+        infer(kb, new String[] { "P31", "P22" });
+    }
+    
+    private void infer(KnowledgeBase kb, String[] queries) {
         StringBuilder sb = new StringBuilder();
 
         sb.append("KB: ").append(kb.toString()).append("\n");
         for (String queryString : queries) {
             boolean valid = kb.askWithTTEntails(queryString);
+            boolean invalid = kb.askWithTTEntails("~" + queryString);
 
             sb.append("  -> ")
               .append(queryString)
               .append(" = ")
-              .append(valid ? "Yes" : "No");
+              .append(valid == invalid ? "?" : valid ? "Yes" : "No");
             sb.append("\n");
         }
         sb.append("\n");
@@ -37,5 +53,6 @@ public class WumpusWorld {
         WumpusWorld ww = new WumpusWorld();
         
         ww.print_7_3_a();
+        ww.print_7_3_b();
     }
 }
