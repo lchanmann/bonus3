6eacf2089419c124dd61544186b3bf8424994991
Mann <chanmannlim@gmail.com>
2014-11-08 16:15:02 -0600

    7.3 a

--- src/main/java/bonus3/WumpusWorld.java
+++ src/main/java/bonus3/WumpusWorld.java
@@ -3,17 +3,39 @@ package bonus3;
 import aima.core.logic.propositional.kb.KnowledgeBase;
 
 public class WumpusWorld {
+    
+    private KnowledgeBase kb = new KnowledgeBase();
+    
+    public void print_7_3_a() {
+        kb.tell("B11 <=> P12 | P21");
+        kb.tell("OK12 <=> ~P12");
+        kb.tell("OK21 <=> ~P21");
+
+        kb.tell("~B11");
+        infer(new String[] { "OK12", "OK21" });
+    }
+    
+    private void infer(String[] queries) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("KB: ").append(kb.toString()).append("\n");
+        for (String queryString : queries) {
+            boolean valid = kb.askWithTTEntails(queryString);
+
+            sb.append("  -> ")
+              .append(queryString)
+              .append(" = ")
+              .append(valid ? "Yes" : "No");
+            sb.append("\n");
+        }
+        sb.append("\n");
+
+        System.out.println(sb.toString());
+    }
 
     public static void main(String[] args) {
-        KnowledgeBase kb = new KnowledgeBase();
+        WumpusWorld ww = new WumpusWorld();
         
-        kb.tell("B11 <=> P12 | P21");
-        kb.tell("~B11");
-        
-        if (kb.askWithTTEntails("~P12")) {
-            System.out.println("[1,2] = OK");
-        } else {
-            System.out.println("[1,2] = NO");
-        }
+        ww.print_7_3_a();
     }
 }
