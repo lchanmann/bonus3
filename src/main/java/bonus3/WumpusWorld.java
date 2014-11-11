508d6a1d0ef4b4864113fb67b2ba237c28bab82a
Mann <chanmannlim@gmail.com>
2014-11-10 18:45:20 -0600

    small clean up

--- src/main/java/bonus3/WumpusWorld.java
+++ src/main/java/bonus3/WumpusWorld.java
@@ -3,7 +3,15 @@ package bonus3;
 import aima.core.logic.propositional.kb.KnowledgeBase;
 
 public class WumpusWorld {
-    
+
+    public static void main(String[] args) {
+        WumpusWorld ww = new WumpusWorld();
+
+        ww.print_7_3_a();
+        ww.print_7_3_b();
+        ww.print_7_4_a();
+    }
+
     public void print_7_3_a() {
         KnowledgeBase kb = new KnowledgeBase();
 
@@ -14,9 +22,10 @@ public class WumpusWorld {
 
         kb.tell("OK11");
         kb.tell("~B11");
-        ttEntails(kb, "OK12", "OK21");
+
+        printEntailments(kb, "OK12", "OK21");
     }
-    
+
     public void print_7_3_b() {
         KnowledgeBase kb = new KnowledgeBase();
 
@@ -24,34 +33,34 @@ public class WumpusWorld {
         kb.tell("OK11 <=> ~P11");
         kb.tell("OK12 <=> ~P12");
         kb.tell("OK21 <=> ~P21");
-        
+
         kb.tell("B21");
         kb.tell("OK11");
-        
-        ttEntails(kb, "P31", "P22");
+
+        printEntailments(kb, "P31", "P22");
     }
     
     public void print_7_4_a() {
         KnowledgeBase kb = new KnowledgeBase();
-        
+
         kb.tell("S12 <=> W11 | W22 | W13");
         kb.tell("B21 <=> P11 | P31 | P22");
         kb.tell("OK11 <=> ~W11 & ~P11");
         kb.tell("OK12 <=> ~W12 & ~P12");
         kb.tell("OK21 <=> ~W21 & ~P21");
         kb.tell("OK22 <=> ~W22 & ~P22");
-        
+
         kb.tell("S12");
+        kb.tell("B21");
         kb.tell("OK11");
         kb.tell("OK12");
         kb.tell("OK21");
         kb.tell("OK22");
-        kb.tell("B21");
-        
-        ttEntails(kb, "W13", "OK22", "P31");
+
+        printEntailments(kb, "W13", "OK22", "P31");
     }
     
-    private void ttEntails(KnowledgeBase kb, String... queries) {
+    private void printEntailments(KnowledgeBase kb, String... queries) {
         StringBuilder sb = new StringBuilder();
 
         sb.append("KB: ").append(kb.toString()).append("\n");
@@ -68,13 +77,5 @@ public class WumpusWorld {
         sb.append("\n");
 
         System.out.println(sb.toString());
-    }
-
-    public static void main(String[] args) {
-        WumpusWorld ww = new WumpusWorld();
-        
-        ww.print_7_3_a();
-        ww.print_7_3_b();
-        ww.print_7_4_a();
     }
 }
